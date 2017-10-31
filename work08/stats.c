ca994632a809116a0e64cfb92a069e01e4a71580
jensenli049 <jensenli049@gmail.com>
2017-10-31 16:46:27 -0400

    added stats files

---
+++ work08/stats.c
@@ -0,0 +1,62 @@
+#include "stats.h"
+
+int main(){
+
+  struct stat s_buff;
+  stat("stats.c",&s_buff);
+  long fSize = s_buff.st_size;
+  int mode = s_buff.st_mode % 01000;
+
+  printf("file size: %ld\n", fSize);//print file size
+  printf("file mode: %o\n", mode);//print mode (permissions)
+  printf("file time: %s\n", ctime(&s_buff.st_atime)); //print time of last access
+
+  //modify size display
+  char sDisplay[50] = {0};
+  while( fSize ){
+    if( fSize >= 1000000){
+      sprintf(sDisplay,"%ld GB ",fSize/1000000);
+      fSize %= 1000000;
+    }
+    else if( fSize >= 1000){
+      sprintf(sDisplay,"%s%ld MB ", sDisplay, fSize/1000);
+      fSize %= 1000;
+    }
+    else{
+      sprintf(sDisplay,"%s%ld B", sDisplay, fSize);
+      fSize = 0;
+    }
+    //printf("fSize: %ld\n", fSize);
+  }
+  printf("file size formatted: %s\n",sDisplay);
+
+  //print in ls -l form
+  char lsMode[50];
+  int i;
+  
+  for(i=3; i>0; i--){
+    //printf("%o\n",mode);
+    if( mode >= 0100 ){
+      sprintf(lsMode, "%s", permission(mode / 0100));
+      mode %= 0100;
+    }
+    else if( mode >= 010 ){
+      sprintf(lsMode, "%s%s", lsMode, permission(mode / 010));
+      mode %= 010;
+    }
+    else sprintf(lsMode, "%s%s", lsMode, permission(mode));
+  }
+  printf("ls -l format: %s\n",lsMode);
+  
+}
+
+char * permission(int n){
+  if( n == 0 ) return "---";
+  if( n == 1 ) return "--x";
+  if( n == 2 ) return "-w-";
+  if( n == 3 ) return "-wx";
+  if( n == 4 ) return "r--";
+  if( n == 5 ) return "r-x";
+  if( n == 6 ) return "rw-";
+  if( n == 7 ) return "rwx";
+}
---
+++ work08/stats.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <string.h>
+
+char * permission(int n);
